0d9e4025b9d91b2911c88d9d32618459a6770d9e
Manan Tank <manantankm@gmail.com>
2020-09-14 12:44:03 +0530

    ADD tests for local state management

---
+++ __test__/getRS-mutationFlag.test.js
@@ -0,0 +1,22 @@
+import getRS from '../utils/getRS'
+import { getOnLSChange } from '../utils/getOnChange'
+
+test('$ is incremented when state (object) is mutated', () => {
+  const RS = { current: null }
+  RS.current = getRS({ a: 0 }, getOnLSChange(RS, () => {}))
+  const state = RS.current
+
+  expect(state.$).toBe(0)
+  state.a++
+  expect(state.$ > 0).toBeTruthy()
+})
+
+test('$ is incremented when state (array) is mutated', () => {
+  const RS = { current: null }
+  RS.current = getRS([200], getOnLSChange(RS, () => {}))
+  const state = RS.current
+
+  expect(state.$).toBe(0)
+  state.push(100)
+  expect(state.$ > 0).toBeTruthy()
+})
--- utils/depsUpdated.js
+++ utils/depsUpdated.js
@@ -1,10 +1,11 @@
 // dep is a string, chains is an array of string
 // if any chain starts with dep string return true
-// examples
 
-// ('a.b', 'a.b.c.d') -> true
-// ('a', 'x') -> false
-// ('a', 'a') -> true
+// examples
+// ('a.b', ['a.b.c.d']) -> true
+// ('a', ['x']) -> false
+// ('a', ['x', 'a.b']) -> true
+// ('a', ['a']) -> true
 const depUpdated = (dep, chains) => chains.some(chain => chain.startsWith(dep))
 
 // if any dep is updated return true
