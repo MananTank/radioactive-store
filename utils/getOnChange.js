98f7e3cf255800cef73d472b67b9b4a768e606ba
Manan Tank <manantankm@gmail.com>
2020-09-14 16:18:01 +0530

    input binding cursor jump fix added for global state

--- utils/getOnChange.js
+++ utils/getOnChange.js
@@ -6,17 +6,19 @@ const getOnGSChange = (store) => {
   const timer = { set: false }
   let chains = []
   const notify = (chain) => store.listeners.forEach(l => l(chain))
+  const updateUI = () => {
+    notify(chains)
+    chains = []
+  }
 
-  const onChange = (chain, value, trap) => {
+  const onChange = (chain, value, trap, updateNow) => {
     const addingObject = typeof value === 'object' && trap === 'set'
     const rValue = addingObject ? getRS(value, onChange, chain) : value
     const success = silentMutate(store.state, chain, rValue, trap)
     if (success) chains.push(chain.join('.'))
+    if (updateNow) updateUI() // for input binding
     if (!timer.set) {
-      afterSync(() => {
-        notify(chains)
-        chains = []
-      }, timer)
+      afterSync(updateUI, timer)
     }
     return success
   }
@@ -31,7 +33,7 @@ const getOnLSChange = (ref, forceUpdate) => {
     const addingObject = typeof value === 'object' && trap === 'set'
     const rValue = addingObject ? getRS(value, onChange, chain) : value
     const success = silentMutate(ref.current, chain, rValue, trap)
-    if (updateNow) forceUpdate()
+    if (updateNow) forceUpdate() // for input binding
     else if (!timer.set) afterSync(forceUpdate, timer)
     return success
   }
