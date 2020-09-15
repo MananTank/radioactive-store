ba55e16e17b079dfea67325e00cb0408db8b1c59
Manan Tank <manantankm@gmail.com>
2020-09-15 12:59:02 +0530

    code refactor in useGS

--- hooks/useGS.js
+++ hooks/useGS.js
@@ -1,18 +1,11 @@
 import { useReducer, useEffect } from 'react'
 import { checkDeps } from '../utils/validity'
-// import depsUpdated from '../utils/depsUpdated'
 
 const useGS = (deps) => {
   const [, forceUpdate] = useReducer(x => x + 1, 0)
-
-  useEffect(() => {
-    checkDeps(deps)
-  }, [])
-
-  // subscribe / unsubscribe to store
-  useEffect(() => window.radioactiveStore.subscribe(forceUpdate, deps), [])
-
-  return window.radioactiveStore.state
+  useEffect(() => checkDeps(deps), [])
+  useEffect(() => window.radioactiveStore.subscribe(deps, forceUpdate), [])
+  return window.GS
 }
 
 export default useGS
